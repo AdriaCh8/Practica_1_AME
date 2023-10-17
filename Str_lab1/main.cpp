32230443116d52a101250b7f79512d93a2e2bcc7
Adria Chipre <adria.chipre@estudiants.urv.cat>
2023-10-17 20:23:02 +0200

    Ya va el printf!!!!!

--- Str_lab1/main.cpp
+++ Str_lab1/main.cpp
@@ -8,19 +8,35 @@
 
 AnalogIn termo(A0);
 
+float calculate_rtherm(int counts) {
+    float result = 100000 * ((65535.0 / counts) - 1);
+    return result;
+}
+
+float calculate_temp(float rtherm) {
+    float result = 1 / (log(rtherm / 100000) / 4250 + 1 / (25 + 273.15)) - 273.15;
+    return result;
+}
 
 // main() runs in its own thread in the OS
 int main()
 {
     while (true) {
         float count;
-        count = termo.read();
-        float resistance;
-        resistance = (100000 * ((65535/count)-1));
-        printf("Hola");
+        count = termo.read_u16();
+        float resistance = calculate_rtherm(count);
+        float temp = calculate_temp(resistance);
+        printf("==================================\n");
+        printf("|       Sensor de Termistor       |\n");
+        printf("|--------------------------------|\n");
+        printf("| Resistencia: %.2f Ohm          |\n", resistance);
+        printf("| Temperatura: %.2f °C           |\n", calculate_temp(resistance));
+        printf("==================================\n");
         ThisThread::sleep_for(3s);
     }
+
 }
+
 
 
 
